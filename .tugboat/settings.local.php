0bd5dd8464c1ba7952fc1c160497f1bba107bb0a
James Sansbury <james@lullabot.com>
2023-01-12 16:53:46 -0500

    Initial tugboat config

---
+++ .tugboat/settings.local.php
@@ -0,0 +1,16 @@
+<?php
+
+$settings['hash_salt'] = getenv('TUGBOAT_REPO_ID');
+
+$databases['default']['default'] = array (
+  'database' => 'tugboat',
+  'username' => 'tugboat',
+  'password' => 'tugboat',
+  'prefix' => '',
+  'host' => 'mysql',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['config_sync_directory'] = '../config/sync';
