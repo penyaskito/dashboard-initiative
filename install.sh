540fd5857e8b415f6512ef085cddbe056ad603ed
Christian Lopez Espinola <penyaskito@computer.org>
2023-03-17 04:38:56 +0000

    Show dashboard local tasks.

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 ddev drush si --yes
 ddev drush upwd admin admin
+
 ddev drush user:role:add administrator admin
 ddev drush cset system.site uuid 05104925-b5ef-447d-a96a-bb6b3eed6182 --yes
 ddev drush entity-delete shortcut_set --yes
 ddev drush cim --yes
 # For restoring shortcuts.
 ddev drush php-eval 'include_once "core/profiles/standard/standard.install"; standard_install();'
+
+ddev drush user:create editor --password=editor
+ddev drush user:role:add content_editor editor
